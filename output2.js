aa9915c05923faba58e79f30d55f097ec485466b
thachkhanhlinh <thachkhanhlinh@gmail.com>
2024-12-06 23:55:06 +0700

    Delete output2.js

--- output2.js
+++
@@ -1,4 +0,0 @@
-[
-        { question: "Theo Quy trình An toàn điện, trường hợp nào Người CHTT được giữ lại PCT khi nghỉ hết ngày làm việc", answer: "Làm việc trong TBA nhiều ngày liên tục." },
-        { question: "Theo Quy trình An toàn điện quy định về nối đất khi có nhiều ĐVCT trong cùng một phạm vi có cắt điện như thế nào", answer: "Mỗi ĐVCT vẫn phải làm nối đất độc lập cho ĐVCT của mình" }
-]
